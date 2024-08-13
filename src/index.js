937407a9d968aab9d66d57000d83170b06ac3789
khardeaditya <khardeaditya240@gmail.com>
2024-08-12 23:03:59 -0700

    start

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {createRoot} from "react-dom/client"
+
+const root= createRoot(document.getElementById("root"));
+
+const Aa= (Props)=>{
+   
+
+    return (<h1>num is </h1>)
+}
+<Aa />
+
+root.render(<div>
+<h1>as </h1> <h1>wewe </h1>
+
+<h1>as </h1> <h1>wewe</h1>
+{45+2}
+
+
+<Aa/>
+<Aa/><Aa/><Aa/><Aa/>
+
+</div>)
